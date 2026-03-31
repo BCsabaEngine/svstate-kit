3a27e031a9271788f3efb53f141e2c42afbc47d3
BCsabaEngine <csaba@balazs-magyar.hu>
2026-03-31 23:31:39 +0200

    fix: deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 	build: {
 		sourcemap: false,
-		chunkSizeWarningLimit: 1000,
-		rollupOptions: {
-			output: {
-				compact: true
-			}
-		}
+		chunkSizeWarningLimit: 1000
 	}
 });
